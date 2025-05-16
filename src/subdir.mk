f10a73d288c7269ae6f74bfdce9ce2cca5786f59
khosta77 <puwerfulpants@gmail.com>
2025-05-16 11:25:13 +0300

    feat: init project

---
+++ src/subdir.mk
@@ -0,0 +1,16 @@
+C_SRCS += \
+./src/main.cpp
+
+C_DEPS += \
+./src/main.d
+
+OBJS += \
+./src/main.o
+
+
+src/%.o: ./src/%.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_STDOUT -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
