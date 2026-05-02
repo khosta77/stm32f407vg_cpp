a636544398a9381ccf17be05eef1bc59b10f1c79
khosta77 <puwerfulpants@gmail.com>
2026-05-03 01:09:03 +0300

    refactor(rtos,sensors): null checks, mutable mutex, Status in IImu
    - Add configASSERT in Mutex/Semaphore/Queue/Task constructors
    - Use while(true) instead of for(;;) in hooks
    - Make CachedSensor mutex mutable for const get()
    - Return driver::Status from IImu::init/read/selfTest
    - MPU6050: check all writeReg results, use Status

--- sdk/rtos/include/rtos/rtos.hpp
+++ sdk/rtos/include/rtos/rtos.hpp
@@ -1,120 +1,165 @@
 #pragma once
 
 #include "FreeRTOS.h"
-#include "task.h"
-#include "semphr.h"
 #include "queue.h"
+#include "semphr.h"
+#include "task.h"
 #include "timers.h"
 
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <utility>
 
-namespace rtos {
+namespace rtos
+{
 
-class Mutex {
+class Mutex
+{
     SemaphoreHandle_t _handle;
+
 public:
-    Mutex() : _handle(xSemaphoreCreateMutex()) {}
-    ~Mutex() { if (_handle) vSemaphoreDelete(_handle); }
-    Mutex(const Mutex&) = delete;
-    Mutex& operator=(const Mutex&) = delete;
+    Mutex() : _handle( xSemaphoreCreateMutex() ) { configASSERT( _handle ); }
 
-    bool lock(TickType_t timeout = portMAX_DELAY) {
-        return xSemaphoreTake(_handle, timeout) == pdTRUE;
+    ~Mutex()
+    {
+        if ( _handle )
+        {
+            vSemaphoreDelete( _handle );
+        }
     }
 
-    void unlock() {
-        xSemaphoreGive(_handle);
-    }
+    Mutex( const Mutex & ) = delete;
+    Mutex &operator=( const Mutex & ) = delete;
+
+    bool lock( TickType_t timeout = portMAX_DELAY ) { return xSemaphoreTake( _handle, timeout ) == pdTRUE; }
+
+    void unlock() { xSemaphoreGive( _handle ); }
 
     SemaphoreHandle_t handle() const { return _handle; }
 };
 
-class LockGuard {
-    Mutex& _mtx;
+class LockGuard
+{
+    Mutex &_mtx;
+
 public:
-    explicit LockGuard(Mutex& m) : _mtx(m) { _mtx.lock(); }
+    explicit LockGuard( Mutex &m ) : _mtx( m ) { _mtx.lock(); }
     ~LockGuard() { _mtx.unlock(); }
-    LockGuard(const LockGuard&) = delete;
-    LockGuard& operator=(const LockGuard&) = delete;
+    LockGuard( const LockGuard & ) = delete;
+    LockGuard &operator=( const LockGuard & ) = delete;
 };
 
-class BinarySemaphore {
+class BinarySemaphore
+{
     SemaphoreHandle_t _handle;
+
 public:
-    BinarySemaphore() : _handle(xSemaphoreCreateBinary()) {}
-    ~BinarySemaphore() { if (_handle) vSemaphoreDelete(_handle); }
-    BinarySemaphore(const BinarySemaphore&) = delete;
-    BinarySemaphore& operator=(const BinarySemaphore&) = delete;
+    BinarySemaphore() : _handle( xSemaphoreCreateBinary() ) { configASSERT( _handle ); }
 
-    bool take(TickType_t timeout = portMAX_DELAY) {
-        return xSemaphoreTake(_handle, timeout) == pdTRUE;
+    ~BinarySemaphore()
+    {
+        if ( _handle )
+        {
+            vSemaphoreDelete( _handle );
+        }
     }
 
-    void give() {
-        xSemaphoreGive(_handle);
-    }
+    BinarySemaphore( const BinarySemaphore & ) = delete;
+    BinarySemaphore &operator=( const BinarySemaphore & ) = delete;
 
-    bool giveFromISR(BaseType_t* pxHigherPriorityTaskWoken = nullptr) {
+    bool take( TickType_t timeout = portMAX_DELAY ) { return xSemaphoreTake( _handle, timeout ) == pdTRUE; }
+
+    void give() { xSemaphoreGive( _handle ); }
+
+    bool giveFromISR( BaseType_t *pxHigherPriorityTaskWoken = nullptr )
+    {
         BaseType_t dummy = pdFALSE;
-        if (!pxHigherPriorityTaskWoken) pxHigherPriorityTaskWoken = &dummy;
-        return xSemaphoreGiveFromISR(_handle, pxHigherPriorityTaskWoken) == pdTRUE;
+        if ( !pxHigherPriorityTaskWoken )
+        {
+            pxHigherPriorityTaskWoken = &dummy;
+        }
+        return xSemaphoreGiveFromISR( _handle, pxHigherPriorityTaskWoken ) == pdTRUE;
     }
 
     SemaphoreHandle_t handle() const { return _handle; }
 };
 
-template<typename T, size_t N>
-class Queue {
+template <typename T, size_t N> class Queue
+{
     QueueHandle_t _handle;
+
 public:
-    Queue() : _handle(xQueueCreate(N, sizeof(T))) {}
-    ~Queue() { if (_handle) vQueueDelete(_handle); }
-    Queue(const Queue&) = delete;
-    Queue& operator=(const Queue&) = delete;
+    Queue() : _handle( xQueueCreate( N, sizeof( T ) ) ) { configASSERT( _handle ); }
 
-    bool send(const T& item, TickType_t timeout = portMAX_DELAY) {
-        return xQueueSend(_handle, &item, timeout) == pdTRUE;
+    ~Queue()
+    {
+        if ( _handle )
+        {
+            vQueueDelete( _handle );
+        }
     }
 
-    bool sendFromISR(const T& item, BaseType_t* pxHigherPriorityTaskWoken = nullptr) {
+    Queue( const Queue & ) = delete;
+    Queue &operator=( const Queue & ) = delete;
+
+    bool send( const T &item, TickType_t timeout = portMAX_DELAY )
+    {
+        return xQueueSend( _handle, &item, timeout ) == pdTRUE;
+    }
+
+    bool sendFromISR( const T &item, BaseType_t *pxHigherPriorityTaskWoken = nullptr )
+    {
         BaseType_t dummy = pdFALSE;
-        if (!pxHigherPriorityTaskWoken) pxHigherPriorityTaskWoken = &dummy;
-        return xQueueSendFromISR(_handle, &item, pxHigherPriorityTaskWoken) == pdTRUE;
+        if ( !pxHigherPriorityTaskWoken )
+        {
+            pxHigherPriorityTaskWoken = &dummy;
+        }
+        return xQueueSendFromISR( _handle, &item, pxHigherPriorityTaskWoken ) == pdTRUE;
     }
 
-    bool receive(T& item, TickType_t timeout = portMAX_DELAY) {
-        return xQueueReceive(_handle, &item, timeout) == pdTRUE;
+    bool receive( T &item, TickType_t timeout = portMAX_DELAY )
+    {
+        return xQueueReceive( _handle, &item, timeout ) == pdTRUE;
     }
 
-    UBaseType_t size() const { return uxQueueMessagesWaiting(_handle); }
+    UBaseType_t size() const { return uxQueueMessagesWaiting( _handle ); }
     bool empty() const { return size() == 0; }
     QueueHandle_t handle() const { return _handle; }
 };
 
-class Task {
+class Task
+{
     TaskHandle_t _handle = nullptr;
+
 public:
-    Task(const char* name, uint16_t stackDepth, UBaseType_t priority,
-         void (*fn)(void*), void* param = nullptr) {
-        xTaskCreate(fn, name, stackDepth, param, priority, &_handle);
+    Task( const char *name, uint16_t stackDepth, UBaseType_t priority, void ( *fn )( void * ),
+          void *param = nullptr )
+    {
+        xTaskCreate( fn, name, stackDepth, param, priority, &_handle );
+        configASSERT( _handle );
     }
 
-    ~Task() {
-        if (_handle) vTaskDelete(_handle);
+    ~Task()
+    {
+        if ( _handle )
+        {
+            vTaskDelete( _handle );
+        }
     }
 
-    Task(const Task&) = delete;
-    Task& operator=(const Task&) = delete;
+    Task( const Task & ) = delete;
+    Task &operator=( const Task & ) = delete;
 
-    Task(Task&& other) noexcept : _handle(other._handle) {
-        other._handle = nullptr;
-    }
+    Task( Task &&other ) noexcept : _handle( other._handle ) { other._handle = nullptr; }
 
-    Task& operator=(Task&& other) noexcept {
-        if (this != &other) {
-            if (_handle) vTaskDelete(_handle);
+    Task &operator=( Task &&other ) noexcept
+    {
+        if ( this != &other )
+        {
+            if ( _handle )
+            {
+                vTaskDelete( _handle );
+            }
             _handle = other._handle;
             other._handle = nullptr;
         }
@@ -123,10 +168,8 @@ public:
 
     TaskHandle_t handle() const { return _handle; }
 
-    static void delay(TickType_t ticks) { vTaskDelay(ticks); }
-    static void delayUntil(TickType_t* prev, TickType_t increment) {
-        vTaskDelayUntil(prev, increment);
-    }
+    static void delay( TickType_t ticks ) { vTaskDelay( ticks ); }
+    static void delayUntil( TickType_t *prev, TickType_t increment ) { vTaskDelayUntil( prev, increment ); }
     static void startScheduler() { vTaskStartScheduler(); }
 };
 
--- sdk/rtos/src/freertos_hooks.c
+++ sdk/rtos/src/freertos_hooks.c
@@ -1,26 +1,32 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-    (void)xTask;
-    (void)pcTaskName;
-    __asm volatile ("cpsid i" ::: "memory");
-    for (;;) {}
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName )
+{
+    (void) xTask;
+    (void) pcTaskName;
+    __asm volatile( "cpsid i" ::: "memory" );
+    while ( 1 )
+    {
+    }
 }
 
-void vApplicationMallocFailedHook(void) {
-    __asm volatile ("cpsid i" ::: "memory");
-    for (;;) {}
+void vApplicationMallocFailedHook( void )
+{
+    __asm volatile( "cpsid i" ::: "memory" );
+    while ( 1 )
+    {
+    }
 }
 
-#if (configSUPPORT_STATIC_ALLOCATION == 1)
+#if ( configSUPPORT_STATIC_ALLOCATION == 1 )
 
 static StaticTask_t xIdleTaskTCB;
 static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
 
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
-                                   StackType_t **ppxIdleTaskStackBuffer,
-                                   uint32_t *pulIdleTaskStackSize) {
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
+                                    uint32_t *pulIdleTaskStackSize )
+{
     *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
     *ppxIdleTaskStackBuffer = uxIdleTaskStack;
     *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
@@ -29,9 +35,9 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
 static StaticTask_t xTimerTaskTCB;
 static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
 
-void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
-                                    StackType_t **ppxTimerTaskStackBuffer,
-                                    uint32_t *pulTimerTaskStackSize) {
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer,
+                                     StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize )
+{
     *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
     *ppxTimerTaskStackBuffer = uxTimerTaskStack;
     *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
