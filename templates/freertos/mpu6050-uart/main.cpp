f6bab72cf23b2ad27246ac8c08dffb396524702f
khosta77 <puwerfulpants@gmail.com>
2026-05-03 19:57:02 +0300

    refactor(drivers,templates): auto clock enable in constructors, global objects
    - GpioPin: enable GPIO port clock in constructor (if/else on port address)
    - I2C: keep clock enable in __initialize_hardware (non-template, works)
    - UART: keep clock enable in __initialize_hardware (template TU-local issue)
    - Templates: global objects instead of pointers + static locals
      - No raw pointers, direct object access (g_led.toggle() not g_led->toggle())
      - __initialize_hardware() override enables I2C/UART clocks before constructors
      - GpioPin self-enables its clock, no manual RCC for GPIO
    - Add detail::dsb() wrapper for __DSB() in UART template (TU-local fix)

--- templates/freertos/mpu6050-uart/main.cpp
+++ templates/freertos/mpu6050-uart/main.cpp
@@ -27,17 +27,32 @@ using driver::stm32f4::GpioPin;
 using driver::stm32f4::I2c;
 using driver::stm32f4::Uart;
 
+extern "C" void __initialize_hardware()
+{
+    SystemCoreClockUpdate();
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIODEN;
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN | RCC_APB1ENR_USART2EN;
+    __DSB();
+}
+
 constexpr float TILT_THRESHOLD = 15.0f;
 constexpr float RAD_TO_DEG = 180.0f / 3.14159265f;
 
 namespace
 {
 
-GpioPin *g_ledGreen;
-GpioPin *g_ledOrange;
-GpioPin *g_ledRed;
-GpioPin *g_ledBlue;
-Uart<> *g_uart2;
+GpioPin g_uartTx{ *GPIOA, { 2, PinMode::AlternateFunction, PullMode::None, OutputSpeed::VeryHigh, OutputType::PushPull, 7 } };
+GpioPin g_uartRx{ *GPIOA, { 3, PinMode::AlternateFunction, PullMode::None, OutputSpeed::VeryHigh, OutputType::PushPull, 7 } };
+GpioPin g_i2cScl{ *GPIOB, { 6, PinMode::AlternateFunction, PullMode::PullUp, OutputSpeed::VeryHigh, OutputType::OpenDrain, 4 } };
+GpioPin g_i2cSda{ *GPIOB, { 7, PinMode::AlternateFunction, PullMode::PullUp, OutputSpeed::VeryHigh, OutputType::OpenDrain, 4 } };
+GpioPin g_ledGreen{ *GPIOD, { 12, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
+GpioPin g_ledOrange{ *GPIOD, { 13, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
+GpioPin g_ledRed{ *GPIOD, { 14, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
+GpioPin g_ledBlue{ *GPIOD, { 15, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
+
+I2c g_i2c1{ *I2C1, { .clockSpeed = 400000, .fastMode = true } };
+Uart<> g_uart2{ *USART2, USART2_IRQn, { .baudrate = 115200, .dataBits = 8, .stopBits = 1, .parity = Parity::None } };
+sensor::Mpu6050 g_mpu{ g_i2c1, { .addr = 0x68, .accelRange = 2, .gyroRange = 250, .sampleRateDiv = 7, .dlpfMode = 6 } };
 
 driver::Status readImu( sensor::ImuData &out, void *ctx )
 {
@@ -63,7 +78,7 @@ void taskView( void *param )
                             ax, ay, az, gx, gy, gz, t );
         if ( len > 0 )
         {
-            g_uart2->write( { reinterpret_cast<const uint8_t *>( buf ),
+            g_uart2.write( { reinterpret_cast<const uint8_t *>( buf ),
                              static_cast<size_t>( len ) } );
         }
         rtos::Task::delay( pdMS_TO_TICKS( 100 ) );
@@ -79,27 +94,27 @@ void taskLed( void *param )
         float pitch = atan2f( data.accel.y, data.accel.z ) * RAD_TO_DEG;
         float roll = atan2f( data.accel.x, data.accel.z ) * RAD_TO_DEG;
 
-        g_ledGreen->reset();
-        g_ledOrange->reset();
-        g_ledRed->reset();
-        g_ledBlue->reset();
+        g_ledGreen.reset();
+        g_ledOrange.reset();
+        g_ledRed.reset();
+        g_ledBlue.reset();
 
         if ( pitch > TILT_THRESHOLD )
         {
-            g_ledGreen->set();
+            g_ledGreen.set();
         }
         else if ( pitch < -TILT_THRESHOLD )
         {
-            g_ledRed->set();
+            g_ledRed.set();
         }
 
         if ( roll > TILT_THRESHOLD )
         {
-            g_ledOrange->set();
+            g_ledOrange.set();
         }
         else if ( roll < -TILT_THRESHOLD )
         {
-            g_ledBlue->set();
+            g_ledBlue.set();
         }
 
         rtos::Task::delay( pdMS_TO_TICKS( 50 ) );
@@ -108,56 +123,16 @@ void taskLed( void *param )
 
 } // namespace
 
-extern "C" void USART2_IRQHandler() { g_uart2->irqHandler(); }
+extern "C" void USART2_IRQHandler() { g_uart2.irqHandler(); }
 
 int main()
 {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIODEN;
-    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN | RCC_APB1ENR_USART2EN;
-    __DSB();
+    g_mpu.init();
 
-    static GpioPin uartTx{ *GPIOA,
-        { 2, PinMode::AlternateFunction, PullMode::None, OutputSpeed::VeryHigh, OutputType::PushPull, 7 } };
-    static GpioPin uartRx{ *GPIOA,
-        { 3, PinMode::AlternateFunction, PullMode::None, OutputSpeed::VeryHigh, OutputType::PushPull, 7 } };
-    (void) uartTx;
-    (void) uartRx;
-
-    static GpioPin i2cScl{ *GPIOB,
-        { 6, PinMode::AlternateFunction, PullMode::PullUp, OutputSpeed::VeryHigh, OutputType::OpenDrain, 4 } };
-    static GpioPin i2cSda{ *GPIOB,
-        { 7, PinMode::AlternateFunction, PullMode::PullUp, OutputSpeed::VeryHigh, OutputType::OpenDrain, 4 } };
-    (void) i2cScl;
-    (void) i2cSda;
-
-    static GpioPin ledGreen{ *GPIOD,
-        { 12, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
-    static GpioPin ledOrange{ *GPIOD,
-        { 13, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
-    static GpioPin ledRed{ *GPIOD,
-        { 14, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
-    static GpioPin ledBlue{ *GPIOD,
-        { 15, PinMode::Output, PullMode::None, OutputSpeed::Low, OutputType::PushPull } };
-
-    g_ledGreen = &ledGreen;
-    g_ledOrange = &ledOrange;
-    g_ledRed = &ledRed;
-    g_ledBlue = &ledBlue;
-
-    static I2c i2c1{ *I2C1, { .clockSpeed = 400000, .fastMode = true } };
-
-    __disable_irq();
-    static Uart<> uart2{ *USART2, USART2_IRQn, { .baudrate = 115200, .dataBits = 8, .stopBits = 1, .parity = Parity::None } };
-    g_uart2 = &uart2;
-    __enable_irq();
-
-    static sensor::Mpu6050 mpu{ i2c1, { .addr = 0x68, .accelRange = 2, .gyroRange = 250, .sampleRateDiv = 7, .dlpfMode = 6 } };
-    mpu.init();
-
-    static sensor::CachedSensor<sensor::ImuData> cachedImu( 10, readImu, &mpu, "imu", 256, 2 );
+    static sensor::CachedSensor<sensor::ImuData> cachedImu( 10, readImu, &g_mpu, "imu", 256, 2 );
 
     static rtos::Task view( "view", 384, 1, taskView, &cachedImu );
-    static rtos::Task led( "led", 256, 1, taskLed, &cachedImu );
+    static rtos::Task led( "led", 384, 1, taskLed, &cachedImu );
 
     rtos::Task::startScheduler();
 
